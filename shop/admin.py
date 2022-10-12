f24f279bc0b5904b8c37b73e778185ff8643ef54
Denchopper <denchopper@gmail.com>
2022-10-12 20:48:39 +0300

    new models

--- shop/admin.py
+++ shop/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import BeerType, Brewery, BeerStyle, SnackType, Beer
+from .models import BeerType, Brewery, BeerStyle, SnackType, Beer, BeerVolume, Options
 
 admin.site.register(BeerType)
 admin.site.register(Brewery)
 admin.site.register(BeerStyle)
 admin.site.register(SnackType)
 admin.site.register(Beer)
+admin.site.register(BeerVolume)
+admin.site.register(Options)
--- shop/models.py
+++ shop/models.py
@@ -5,29 +5,57 @@ class BeerType(models.Model):  # Пиво: розливне|пляшкове
     name = models.CharField(max_length=50)  # "Тип пива"
     description = models.TextField()  # "Опис"
 
+    def __str__(self):
+        return self.name
+
 
 class Brewery(models.Model):  # Броварні
     name = models.CharField(max_length=50)  # 'Назва броварні'
     description = models.TextField()  # 'Опис броварні'
 
+    def __str__(self):
+        return self.name
+
+
+class BeerVolume(models.Model):
+    name = models.CharField(default='0', max_length=50)
+
+    def __str__(self):
+        return self.name
+
 
 class BeerStyle(models.Model):  # Стиль пива: IPA/Stout|etc.
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 
 class SnackType(models.Model):  # Тип закуски: холодна|гаряча
     name = models.CharField(max_length=50)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 
 class Beer(models.Model):
     brewery = models.ForeignKey(Brewery, on_delete=models.CASCADE)
     beer_style = models.ForeignKey(BeerStyle, on_delete=models.CASCADE)  # 'Стиль'
+    name = models.CharField(default=" ", max_length=30)
     description = models.TextField()
     abv = models.PositiveSmallIntegerField()  # 'ABV (Відсоток алкоголю)'
     og = models.PositiveSmallIntegerField()  # 'OG (Щільність)'
     ibu = models.PositiveSmallIntegerField()  # 'IBU (Гіркота)'
-    price = models.PositiveSmallIntegerField()  # 'Ціна за 1 літр'
+    volume = models.ManyToManyField(BeerVolume, through="Options")
 
+    def __str__(self):
+        return self.name
+
+
+class Options(models.Model):  # Варіації пива та об'єму
+    beer = models.ForeignKey(Beer, on_delete=models.CASCADE)
+    volume = models.ForeignKey(BeerVolume, on_delete=models.CASCADE)
+    price = models.PositiveSmallIntegerField()
 
 
